aae5aae77da962bb4ac598dc3d72a16a7685efef
Tamas Somhegyi <somhegyi.tamas@gmail.com>
2014-07-17 08:20:54 +0200

    Eclipse project is created

---
+++ src/feladat01/Hello.java
@@ -0,0 +1,14 @@
+package feladat01;
+
+/* 1. Készíts "Hello World" alkalmazást! (main metódusba kiírás) */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+    
+    public void sayHello() {
+        
+    }
+
+}
