47964ae50c0ef0f0d3ae76c89ffe1c703f1cf17d
Tamas Somhegyi <somhegyi.tamas@gmail.com>
2014-07-17 12:27:24 +0200

    feladatsor01.feladat01 is completed

--- src/feladatsor01/feladat01/Hello.java
+++ src/feladatsor01/feladat01/Hello.java
@@ -1,14 +1,11 @@
-package feladat01;
+package feladatsor01.feladat01;
 
 /* 1. Készíts "Hello World" alkalmazást! (main metódusba kiírás) */
+
 public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-    }
-    
-    public void sayHello() {
-        
     }
 
 }
---
+++ src/feladatsor01/feladat02/MainHello.java
@@ -0,0 +1,19 @@
+package feladatsor01.feladat02;
+
+/** 2. Helyezd el a "Hello World" kiírását egy példánymetódusba. Ekkor a main metódusban meg kell példányosítani
+   az osztályodat és meghívni rajta a kiíró metódust. */
+
+public class MainHello {
+
+    public static void main(String[] args) {
+        Hello hello = new Hello();
+        hello.sayHello();
+    }
+
+}
+
+class Hello {
+    public void sayHello() {
+        System.out.println("Hello, World!");
+    }
+}
