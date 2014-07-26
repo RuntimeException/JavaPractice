d13865f4201f8c1bdfe9728a0dd0ff6540a053ff
Tamás Somhegyi <somhegyi.tamas@gmail.com>
2014-07-26 17:01:25 +0200

    New feladat is completed

--- src/feladatsor03/feladat04/OddEvenThreads.java
+++ src/feladatsor03/feladat04/OddEvenThreads.java
@@ -1,12 +1,58 @@
 package feladatsor03.feladat04;
 
 /**
- * 4. Készíts két szálat, az egyik páros, a másik páratlan számokat írjon ki fél 
- *  másodpercenként! Oldd meg megbízhatóan, hogy egyszerre csak az egyik szál
- *  futhasson! Készíts a programból jar-t az IDE szolgáltatásait igénybe véve!
- *  Futtasd a programot a parancssorból!
+ * 4. Készíts két szálat, az egyik páros, a másik páratlan számokat írjon ki fél
+ * másodpercenként! Oldd meg megbízhatóan, hogy egyszerre csak az egyik szál
+ * futhasson! Készíts a programból jar-t az IDE szolgáltatásait igénybe véve!
+ * Futtasd a programot a parancssorból!
  */
 
 public class OddEvenThreads {
-	
+    static public Integer i = 1;
+
+    static class OddThread extends Thread {
+
+        @Override
+        public void run() {
+            while (true) {
+                synchronized(i){
+                    if( (i++ & 0x1) != 0 ){
+                        System.out.println("OddThread: " + i);
+                        try {
+                            OddThread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+        
+    }
+
+    static class EvenThread extends Thread {
+
+        @Override
+        public void run() {
+            while (true) {
+                synchronized(i){
+                    if( (i++ & 0x1) == 0 ){
+                        System.out.println("OddThread: " + i);
+                        try {
+                            OddThread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new OddEvenThreads.OddThread().start();
+        new OddEvenThreads.EvenThread().start();
+
+    }
+
 }
