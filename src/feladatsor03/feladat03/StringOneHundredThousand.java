906ca49ad961fca17a3c6e13195a0008bf1175ca
Tamás Somhegyi <somhegyi.tamas@gmail.com>
2014-07-27 15:22:06 +0200

    Minor update

--- src/feladatsor03/feladat03/StringOneHundredThousand.java
+++ src/feladatsor03/feladat03/StringOneHundredThousand.java
@@ -1,19 +1,40 @@
 package feladatsor03.feladat03;
 
 /**
- * 2. Írj végtelen ciklust "while(true)"; Sorsolj véletlenszámokat a ciklus belsejében 
- *    (a Random beépített osztálynak van egy nextInt() metódusa)! Tegyél erre az 
- *    utasításra breakpointot! A debugger akkor álljon meg az adott soron, ha, a 
- *    véletlenszám egy elõre meghatározott tartományban van, pl.: nagyobb, mint 
- *    Integer.MAX_VALUE/5
+ * 3. Add össze 100 000-szer az "abcd" String-et! Mit tapasztalsz? Próbáld meg a
+ * StringBuilder osztállyal!
  */
 
 public class StringOneHundredThousand {
-	static int x;
-	
+    public String text;
+
+    public String lameStringAdder(String str, int count) {
+        String strTemp = str;
+        
+        for (int i = 1; i < count; i++) {
+            strTemp += str;
+        }
+        return strTemp;
+    }
+    
+    public String superStringAdder(String str, int count) {
+        StringBuilder strBldr = new StringBuilder(str);
+        
+        for (int i = 1; i < count; i++) {
+            strBldr.append(str);
+        }
+        return strBldr.toString();
+    }
+
     public static void main(String[] args) {
-    	while(true){
-    		x = (int) (Math.random() * Integer.MAX_VALUE);
-    	}
+        StringOneHundredThousand sot = new StringOneHundredThousand();
+        
+        System.out.println("lameStringAdder - Start");
+        sot.text = sot.lameStringAdder("Motherfucker!!!", 100000);
+        System.out.println("lameStringAdder - End");
+        
+        System.out.println("superStringAdder - Start");
+        sot.text = sot.superStringAdder("Motherfucker!!!", 100000);
+        System.out.println("superStringAdder - End");
     }
 }
